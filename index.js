9486f27dd535aa3e2777739bfad46e6d2fce4b96
Preeti Gusain <preetigusain@Preetis-MacBook-Air.local>
2024-11-20 13:07:52 +0530

    Added logic for checking env. variables

---
+++ index.js
@@ -0,0 +1,26 @@
+const dotenv = require('dotenv');
+
+function checkEnv(requiredVariables) {
+    //Loading environment variable from .env file
+    const result = dotenv.config;
+
+    if(result.error) {
+        console.error("Error: unable to load .env file");
+        process.exit(1);
+    }
+
+    const missingVariables = requiredVariables.filter(
+        (variable) => !process.env[variable] || process.env[variable].trim() == ''
+    );
+
+    if(missingVariables.length > 0) {
+        console.log("Error: Missing required environment variables:");
+        missingVariables.forEach((variable) => {
+            console.log(`- ${variable}`);
+        });
+        process.exit(1);
+    }
+    console.log("All required environment variables are set.");
+};
+
+module.exports = checkEnv;
